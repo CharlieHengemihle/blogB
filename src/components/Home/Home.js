d5ca28b92f31af06d02b4fb8173fcff388b11995
Charlie Hengemihle <charliehengemihle@gmail.com>
2023-01-11 12:58:42 -0700

    fonts changing and previewing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,13 +9,14 @@ import { useState } from 'react';
 export default function Home() {
   const [title, setTitle] = useState('');
   const [subtitle, setSubtitle] = useState('');
+  const [font, setFont] = useState('monteserrat');
   // add useState calls here for title, subtitle, font, align, and text
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview {...{ title, subtitle }} />
-      <Editor {...{ title, setTitle, subtitle, setSubtitle }} />
+      <Preview {...{ title, subtitle, font }} />
+      <Editor {...{ title, setTitle, subtitle, setSubtitle, font, setFont }} />
     </main>
   );
 }
--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview({ title, subtitle }) {
+export default function Preview({ title, subtitle, font }) {
   return (
-    <div className={`preview ${title}`}>
+    <div className={`preview ${font}`}>
       <h1> {title}</h1>
       <span> {subtitle}</span>
     </div>
