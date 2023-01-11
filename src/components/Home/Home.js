4bc7234ddbe76120b2c9584fcf052ed9b475125f
Charlie Hengemihle <charliehengemihle@gmail.com>
2023-01-11 12:45:00 -0700

    previewing title via editor?

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,15 +4,17 @@ import Preview from '../Preview/Preview';
 import Editor from '../Editor/Editor';
 
 import './Home.css';
+import { useState } from 'react';
 
 export default function Home() {
+  const [title, setTitle] = useState('');
   // add useState calls here for title, subtitle, font, align, and text
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview {...{ title }} />
+      <Editor {...{ title, setTitle }} />
     </main>
   );
 }
